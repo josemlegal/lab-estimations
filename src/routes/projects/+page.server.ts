279033948e66a791fb6dca4de0412c0201b85507
josemlegal <josemlegal97@gmail.com>
2023-03-04 14:56:01 -0300

    add create project method

---
+++ src/routes/projects/+page.server.ts
@@ -0,0 +1,33 @@
+import { fail, type Actions, type ServerLoad } from '@sveltejs/kit';
+import { prisma } from '../../lib/server/prisma';
+
+export const load: ServerLoad = async () => {
+	return {
+		projects: await prisma.project.findMany()
+	};
+};
+
+export const actions: Actions = {
+	createProject: async ({ request }) => {
+		const { title, description } = Object.fromEntries(await request.formData()) as {
+			title: string;
+			description: string;
+		};
+
+		try {
+			await prisma.project.create({
+				data: {
+					title,
+					description
+				}
+			});
+		} catch (err) {
+			console.error(err);
+			return fail(500, { message: 'Could not create the article.' });
+		}
+
+		return {
+			status: 201
+		};
+	}
+};
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { PageData } from '../projects/$types';
+
+	export let data: PageData;
+
+	$: ({ projects } = data);
+	import '@picocss/pico';
+</script>
+
+<div class="grid">
+	<div>
+		<h2>Projects:</h2>
+		{#each projects as project}
+			<project>
+				<header>
+					{project.title}
+				</header>
+				<p>
+					{project.description}
+				</p>
+				<form action="?/deleteproject&id={project.id}" method="POST">
+					<button type="submit" class="outline secondary">Go to Epics</button>
+				</form>
+				<a href="/project/{project.id}" role="button" class="outline constrast" style="width: 100%;"
+					>Edit project</a
+				>
+			</project>
+		{/each}
+	</div>
+	<form action="?/createProject" method="POST">
+		<h3>New Project</h3>
+		<label for="title"> Title </label>
+		<input type="text" id="title" name="title" />
+		<label for="description"> Description </label>
+		<textarea id="description" name="description" rows={5} />
+		<button type="submit">Add Project</button>
+	</form>
+</div>

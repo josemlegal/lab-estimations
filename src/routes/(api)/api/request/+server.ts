af7977c02fa97c775ef148ff9429ef8c69b4bb0d
josemlegal <josemlegal97@gmail.com>
2023-03-07 14:56:22 -0300

    Add the delete api method

---
+++ src/routes/(api)/api/request/+server.ts
@@ -0,0 +1,29 @@
+import { fail, type RequestHandler } from '@sveltejs/kit';
+
+export const DELETE = (async ({ url }) => {
+	const id = url.searchParams.get('id');
+
+	console.log('si');
+
+	try {
+		await prisma.request.update({
+			where: {
+				id: Number(id)
+			},
+			data: {
+				deleteStatus: true,
+				deletedAt: new Date()
+			}
+		});
+
+		return new Response(
+			JSON.stringify({
+				code: 201,
+				message: 'Request deleted'
+			})
+		);
+	} catch (err) {
+		console.error(err);
+		return fail(500, { message: 'Could not delete the request' });
+	}
+}) as RequestHandler;

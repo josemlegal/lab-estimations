a3def7567fc056f132e46a272c0bcbf481b802e2
josemlegal <josemlegal97@gmail.com>
2023-03-08 17:29:36 -0300

    Add the Request Page and Get Issues, Project function

--- src/routes/projects/[projectId]/requests/[requestId]/+page.server.ts
+++ src/routes/projects/[projectId]/requests/[requestId]/+page.server.ts
@@ -8,7 +8,8 @@ import type { Issue } from '$lib/types/issue';
 export const load: ServerLoad = async ({ params }) => {
 	return {
 		request: await getRequest(Number(params.requestId)),
-		issues: await getIssues(Number(params.requestId))
+		issues: await getIssues(Number(params.requestId)),
+		project: await getProject(Number(params.projectId))
 	};
 };
 
@@ -49,3 +50,20 @@ async function getIssues(requestId: number) {
 	console.log(JSON.stringify(issues));
 	return issues;
 }
+
+const getProject = async (projectId: number) => {
+	const project: Project = await prisma.project.findUnique({
+		where: {
+			id: projectId
+		},
+		select: {
+			id: true,
+			title: true,
+			description: true
+		}
+	});
+	if (!project) {
+		throw error(404, { message: 'Project not found' });
+	}
+	return project;
+};
--- src/routes/projects/[projectId]/requests/[requestId]/+page.svelte
+++ src/routes/projects/[projectId]/requests/[requestId]/+page.svelte
@@ -1,7 +1,71 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import {
+		Badge,
+		Button,
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	$: ({ project, issues, request } = data);
 </script>
 
-<h1>pagina del request</h1>
+<h1>
+	{request.title}
+	<!-- <a href="/projects/{project.id}/edit">Edit</a> -->
+	<p class="text-sm">{request.description}</p>
+</h1>
+<div>
+	<Button
+		on:click={() => {
+			goto(`${request.id}/issues/create`);
+		}}>Add a new Issue</Button
+	>
+</div>
+
+<Table striped={true}>
+	<TableHead>
+		<TableHeadCell>ISSUES</TableHeadCell>
+	</TableHead>
+	<TableBody tableBodyClass="divide-y">
+		{#each issues as issue}
+			<TableBodyRow>
+				<!-- <TableBodyCell
+					class="cursor-pointer"
+					on:click={() => {
+						goto(`${project.id}/requests/${request.id}`);
+					}}
+				> -->
+				<span>
+					{issue.title}
+				</span>
+				<!-- </TableBodyCell> -->
+
+				<TableBodyCell>
+					<span>{issue.description}</span>
+				</TableBodyCell>
+
+				<TableBodyCell>
+					<!-- <a href="/projects/{project.id}/requests/{request.id}/edit">Edit</a> -->
+				</TableBodyCell>
+
+				<TableBodyCell>
+					<!-- <button
+						on:click={() => {
+							onDeleteRequest(request);
+						}}
+						class="font-medium text-white hover:underline"
+					>
+						Delete
+					</button> -->
+				</TableBodyCell>
+			</TableBodyRow>
+		{/each}
+	</TableBody>
+</Table>
